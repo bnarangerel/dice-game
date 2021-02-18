916a3fc99dc15573ad31c15737fb73874716d233
narangerel <naraa.hikari1214@gmail.com>
2021-02-18 19:52:54 +0800

    Shoog shiddeg bolloo

--- app.js
+++ app.js
@@ -6,14 +6,20 @@ var score = [0, 0];
 //Тоглогчийн ээлжийн оноог хадгалах хувьсагч
 var roundScore = 0;
 
-//Шооны тоог хадгалах хувьсагч //// Санамсаргүйгээр 1-6г буулгана
-var dice = Math.floor(Math.random() * 6) + 1;
-//тоглогчийн онооны оронд шооны дугаарыг тавих
-
-
 //Програм эхлэх
-window.document.querySelector("#score-0").textContent = "0";
-window.document.querySelector("#score-1").textContent = "0";
-window.document.querySelector("#current-0").textContent = "0";
-window.document.querySelector("#current-1").textContent = "0";
+window.document.getElementById("score-0").textContent = "0";
+window.document.getElementById("score-1").textContent = "0";
+
+window.document.getElementById("current-0").textContent = "0";
+window.document.getElementById("current-1").textContent = "0";
+
 window.document.querySelector(".dice").style.display = "none";
+
+//Roll dice дээр дарахад шоо өөрөө санамсаргүйгээр орхигдох хэсэг
+var diceDom = document.querySelector(".dice");
+window.document.querySelector(".btn-roll").addEventListener("click", function(){
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + diceNumber + ".png";
+});
+//Тоглогчийн ээлжийг солих
