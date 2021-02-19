d7f607237a4eeabfd97f80771ead7439b0ff115d
narangerel <naraa.hikari1214@gmail.com>
2021-02-19 14:24:56 +0800

    Togloomoo buren hiij duuslaa

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ var score;
 var roundScore;
 //ялсан тоглогч
 var winner = 0;
+//тоглоомын төлөв
+var isNewGame;
                               //Програм эхлэх
+start();
 function start(){
+    isNewGame = true;
     activePlayer = 0;
     score= [90,0];
     roundScore = 0;
@@ -26,7 +30,6 @@ function start(){
     document.querySelector(".player-0-panel").classList.add("active");
     document.querySelector(".player-1-panel").classList.remove("active");
 }
-start();
                                //Ээлж солих
 function eeljSolih(){
     //current score цуцлагдана 
@@ -39,46 +42,51 @@ function eeljSolih(){
     //active байгаа тоглогчийг солих
     document.querySelector(".player-"+ activePlayer +"-panel").classList.add("active");
 }
-                               //ROLL DICE дээр дарахад шоо шидэх хэсэг
+                            //ROLL DICE дээр дарахад шоо шидэх хэсэг
 var diceDom = document.querySelector(".dice");
+
 document.querySelector(".btn-roll").addEventListener("click", function(){
-    //санамсаргүйгээр 1-6 тоо гарч ирнэ
-    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    if(isNewGame){
+        //санамсаргүйгээр 1-6 тоо гарч ирнэ
+        var diceNumber = Math.floor(Math.random() * 6) + 1;
+    
+        //Шооны зураг вэб дээр гарч ирэх хэсэг
+        diceDom.style.display = "block";
+    
+        //Санамсаргүй тоонд харгалзах шооны зураг гарч ирэх
+        diceDom.src = "dice-" + diceNumber + ".png";
+    
+        //Current score цуглуулах хэсэг
+    
+        if(diceNumber !== 1){
+            //Буусан тоо 1-с өөр бол current scoreг нэмэгдүүлнэ
+            roundScore += diceNumber;
+            document.getElementById("current-" + activePlayer).textContent = roundScore;
+        } else {
+            //Тоглогчдын ээлж солигдоно
+            eeljSolih();
+        }
+    } else alert("GAME IS OVER!!!!\n Please, enter New Game")
+});                         
 
-    //Шооны зураг вэб дээр гарч ирэх хэсэг
-    diceDom.style.display = "block";
-
-    //Санамсаргүй тоонд харгалзах шооны зураг гарч ирэх
-    diceDom.src = "dice-" + diceNumber + ".png";
-
-    //Current score цуглуулах хэсэг
-
-    if(diceNumber !== 1){
-        //Буусан тоо 1-с өөр бол current scoreг нэмэгдүүлнэ
-        roundScore += diceNumber;
-        document.getElementById("current-" + activePlayer).textContent = roundScore;
-    } else {
-        //Тоглогчдын ээлж солигдоно
-        eeljSolih();
-    }
-});
                                     //HOLD товч дарсан үед
 document.querySelector(".btn-hold").addEventListener("click", function(){
-    //үндсэн онооны хэсэгт current оноог шилжүүлэх
-    score[activePlayer] += roundScore;
-    document.getElementById("score-" + activePlayer).textContent = score[activePlayer]; 
-   //Тоглогчийн оноо 100с их болвол хожно
-    if(score[activePlayer] >= 100){
-        winner = activePlayer;
-        document.getElementById("name-" + activePlayer).textContent = "WINNER!";
-        document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
-        document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
-        
-    } else
-        //hold товчийг дарснаар current оноо 0 болно
-        //тоглогчидын ээлж солигдоно
-        eeljSolih();
+    if(isNewGame){
+        //үндсэн онооны хэсэгт current оноог шилжүүлэх
+        score[activePlayer] += roundScore;
+        document.getElementById("score-" + activePlayer).textContent = score[activePlayer]; 
+        //Тоглогчийн оноо 100с их болвол хожно
+        if(score[activePlayer] >= 100){
+            winner = activePlayer;
+            document.getElementById("name-" + activePlayer).textContent = "WINNER!";
+            document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
+            document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+            isNewGame = false;
+        } else
+            //hold товчийг дарснаар current оноо 0 болно
+            //тоглогчидын ээлж солигдоно
+            eeljSolih();
+    } else alert("GAME IS OVER!!!!\n Please, enter New Game")
 });
-
                                     //NEW GAME товч дарсан үед
 document.querySelector(".btn-new").addEventListener("click", start);
